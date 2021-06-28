545e0224ee1cc7a64bcd5f11598787c7c44a2133
Manraj Singh <mn697903@dal.ca>
2021-06-27 23:52:06 -0300

    Port Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const http = require('http'); // same as to "import http from 'http'; "
 
-const port  = 2000 || process.env.port; //if the port is busy then use the global variable defined port
+const port  = process.env.port || 3000; //if the port is busy then use the global variable defined port
 
 
 const server = http.createServer(app);
