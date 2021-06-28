9679ba3027b63293fcc75fd2b6662d2b18378dd0
Manraj Singh <mn697903@dal.ca>
2021-06-28 00:42:52 -0300

    New

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const app = require('./app');
 
-const http = require('http'); // same as to "import http from 'http'; "
 
-const port  = process.env.port || 3000; //if the port is busy then use the global variable defined port
-
-
-const server = http.createServer(app);
-
-server.listen(port, () =>{
-    console.log("Server Started at http://localhost:"+port);
-});
 
 
 
