cff34e92fd46e7f30780a6c2c6bbdb9b0a8632e6
Manraj Singh <mn697903@dal.ca>
2021-07-18 07:46:10 -0300

    Homepage Backend Established

--- index.js
+++ index.js
@@ -1,163 +1,206 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var uniqid = require("uniqid");
+//var uniqid = require("uniqid");
 var app = express();
+var mysql = require('mysql');
+ 
+var con = mysql.createConnection({
+  host: "db-admin.cbsrzgbgkhst.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "admin1234",
+  database: "db_admin"
+});
 
-var users = [{
-    email : 'abc@abc.ca',
-    firstName : 'ABC',
-    id : '5abf6783'
-    },
-    {
-    email : 'xyz@xyz.ca',
-    firstName: 'XYZ',
-    id : '5abf674563'
-    }]
+con.connect(function(err) {
+    if (err) throw err;
+  
+   console.log('connection successful');
+});
 
-let port = process.env.PORT || 3000
+// var users = [{
+//     email : 'abc@abc.ca',
+//     firstName : 'ABC',
+//     id : '5abf6783'
+//     },
+//     {
+//     email : 'xyz@xyz.ca',
+//     firstName: 'XYZ',
+//     id : '5abf674563'
+//     }]
+
+let port = process.env.PORT || 2007
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
-
-
-
-app.post('/add', (req, res) => {
-    const userTemp = req.body;
-    userTemp.id = uniqid();
-    console.log("Inside POST Statement");
-    if(!userTemp || !userTemp.firstName || !userTemp.email){
-        return res.status(404).json({
-            success : 'False',
-            Message : 'Data is incomplete'
-        });
-    }
-    else{
-        users.push(userTemp);
-        return res.status(200).json({
-            success : true,
-            Message : 'User added'
-        });
-    }
-    
-    
-});
-
-app.get('/users', (req, res) => {
-    console.log("Inside GET Statement");
-
-    try{
-
-        if(!users || !users.length){
-            return res.status(404).json({
-                success : 'False',
-                Message : 'Users Not Found'
-            });
-        }
-
-        res.status(200).json({
-            success :  true,
-            Message : "Users retrieved",
-            data : users
-        });
-    }
-    catch(err){
-        return res.status(500).json({
-            success : 'False',
-            Message : 'Server response Error',
-            Stack_Trace : err 
-        })
-
-    }
-    
-    
-});
-
-
-app.get('/user/:id', (req, res) => {
-    const id = req.params.id;
-    var userTemp = null;
-    users.map(user => {
-        if(user.id === id){
-            userTemp = user;
-        }
+app.get('/homepage',(req,res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Homepage",
     });
-    console.log("Inside GET Statement");
-
-    try{
-
-        if(!userTemp){
-            return res.status(404).json({
-                success : 'False',
-                Message : 'User with id = '+ id +' Not Found'
-            });
-        }
-
-        res.status(200).json({
-            success :  true,
-            Message : "Users Received",
-            data : userTemp
-        });
-    }
-    catch(err){
-        return res.status(500).json({
-            success : 'False',
-            Message : 'Server response Error',
-            Stack_Trace : err 
-        })
-
-    }
 });
 
-app.put('/update/:id', (req, res) => {
-    const id = req.params.id;
-    const newUser = req.body;
-    var userTemp = null;
-    users.map(user => {
-        if(user.id === id){
-            if(newUser.firstName){
-                user.firstName = newUser.firstName;
-            }
-            if(newUser.email){
-                user.email = newUser.email;
-            }
+app.get('/homepage/items', (req, res) => {
+
+        con.query(`SELECT * FROM item_details`, function(err, result, fields) {
+            console.log(result.length);
             
-            userTemp = user;
-        }
-    });
-    console.log("Inside PUT Statement");
-
-    try{
-
-        if(!userTemp){
-            return res.status(404).json({
-                success : 'False',
-                Message : 'User with id = '+ id +' Not Found'
-            });
-        }
-        if(!newUser){
-            return res.status(404).json({
-                success : 'False',
-                Message : 'No Body passed in json file'
-            });
-        }
-
-        res.status(200).json({
-            success :  true,
-            Message : "User updated"
+            if (err)
+            {
+                return res.status(404).json({
+                    success: false,
+                    message: "No user found!"
+                })
+            } 
+            if (result)
+            {
+                return res.status(200).json({
+                    success: true,
+                    message: "Users retrieved",
+                    data: result
+                });
+            } 
         });
-    }
-    catch(err){
-        return res.status(500).json({
-            success : 'False',
-            Message : 'Server response Error',
-            Stack_Trace : err 
-        })
 
-    }
 });
+
+
+// app.post('/add', (req, res) => {
+//     const userTemp = req.body;
+//     userTemp.id = uniqid();
+//     console.log("Inside POST Statement");
+//     if(!userTemp || !userTemp.firstName || !userTemp.email){
+//         return res.status(404).json({
+//             success : 'False',
+//             Message : 'Data is incomplete'
+//         });
+//     }
+//     else{
+//         users.push(userTemp);
+//         return res.status(200).json({
+//             success : true,
+//             Message : 'User added'
+//         });
+//     }
+    
+    
+// });
+
+// app.get('/users', (req, res) => {
+//     console.log("Inside GET Statement");
+
+//     try{
+
+//         if(!users || !users.length){
+//             return res.status(404).json({
+//                 success : 'False',
+//                 Message : 'Users Not Found'
+//             });
+//         }
+
+//         res.status(200).json({
+//             success :  true,
+//             Message : "Users retrieved",
+//             data : users
+//         });
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             success : 'False',
+//             Message : 'Server response Error',
+//             Stack_Trace : err 
+//         })
+
+//     }
+    
+    
+// });
+
+
+// app.get('/user/:id', (req, res) => {
+//     const id = req.params.id;
+//     var userTemp = null;
+//     users.map(user => {
+//         if(user.id === id){
+//             userTemp = user;
+//         }
+//     });
+//     console.log("Inside GET Statement");
+
+//     try{
+
+//         if(!userTemp){
+//             return res.status(404).json({
+//                 success : 'False',
+//                 Message : 'User with id = '+ id +' Not Found'
+//             });
+//         }
+
+//         res.status(200).json({
+//             success :  true,
+//             Message : "Users Received",
+//             data : userTemp
+//         });
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             success : 'False',
+//             Message : 'Server response Error',
+//             Stack_Trace : err 
+//         })
+
+//     }
+// });
+
+// app.put('/update/:id', (req, res) => {
+//     const id = req.params.id;
+//     const newUser = req.body;
+//     var userTemp = null;
+//     users.map(user => {
+//         if(user.id === id){
+//             if(newUser.firstName){
+//                 user.firstName = newUser.firstName;
+//             }
+//             if(newUser.email){
+//                 user.email = newUser.email;
+//             }
+            
+//             userTemp = user;
+//         }
+//     });
+//     console.log("Inside PUT Statement");
+
+//     try{
+
+//         if(!userTemp){
+//             return res.status(404).json({
+//                 success : 'False',
+//                 Message : 'User with id = '+ id +' Not Found'
+//             });
+//         }
+//         if(!newUser){
+//             return res.status(404).json({
+//                 success : 'False',
+//                 Message : 'No Body passed in json file'
+//             });
+//         }
+
+//         res.status(200).json({
+//             success :  true,
+//             Message : "User updated"
+//         });
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             success : 'False',
+//             Message : 'Server response Error',
+//             Stack_Trace : err 
+//         })
+
+//     }
+// });
 
 
 
