259ae8c5f627106aa1e77e8b22d228ad1928f872
Narendran K <47205680+narencsp@users.noreply.github.com>
2021-07-31 19:20:00 -0400

    Update AdminBackend.js

--- api/routes/AdminBackend.js
+++ api/routes/AdminBackend.js
@@ -19,6 +19,11 @@ const db = mysql.createPool({
 
 
 
+apps.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://group10proposalweb.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //defaults
 apps.use(cors());
